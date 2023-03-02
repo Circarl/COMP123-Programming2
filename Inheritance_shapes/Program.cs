5d382ca937238f82ce8fdb1d6d38e96f41a7caa1
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-02 09:56:16 -0500

    inheritance study sample

--- Inheritance_shapes/Program.cs
+++ Inheritance_shapes/Program.cs
@@ -1,20 +1,14 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
 
 
 namespace Week05
 
 {
-
     #region McDonalds
-
     //consider McDonalds -> server, supervisor, storemanager
 
     class Server
@@ -36,16 +30,12 @@ namespace Week05
             duties = new List<string>() {
 
             "Take order",
-
             "Take cash",
-
             "Deliver order",
-
             "Clean table",
-
             "Mop floor"
 
-};
+            };
 
         }
 
@@ -56,37 +46,19 @@ namespace Week05
     }
 
     class Supervisor : Server
-
     {
-
-        public Supervisor(string name)
-
-        : base(name)
-
+        public Supervisor(string name) : base(name)
         {
-
             wages = 19.99;
-
             duties.Add("Schedule Counter Attendents");
-
         }
-
     }
-
     class Manager : Supervisor
-
     {
-
-        public Manager(string name)
-
-        : base(name)
-
+        public Manager(string name) : base(name)
         {
-
             wages = 25.50;
-
             duties.Add("Maintain Inventory");
-
         }
 
     }
@@ -101,45 +73,29 @@ namespace Week05
     {
 
         public double Length { get; }
-
         public double Width { get; }
-
         public abstract double Area { get; }//no implementation possible
 
         public Figure2D(double length, double width)
-
         => (Length, Width) = (length, width);
 
-        public override string ToString()
-
-        => $"[{Length},{Width}] area: {Area}";
+        public override string ToString() => $"[{Length},{Width}] area: {Area}";
 
     }
 
     class Triangle : Figure2D
-
     {
-
         public override double Area
-
         {
-
             get
-
             {
-
                 return Length * Width * 0.5;
-
             }
 
         }
 
-        public Triangle(double length, double width)
-
-        : base(length, width)
-
+        public Triangle(double length, double width) : base(length, width)
         { }
-
         public override string ToString()
 
         => $"Triangle: {base.ToString()}";
@@ -147,34 +103,19 @@ namespace Week05
     }
 
     class Rectangle : Figure2D
-
     {
-
         public override double Area { get => Length * Width; }//computed property
-
-        public Rectangle(double length, double width)
-
-        : base(length, width)
-
+        public Rectangle(double length, double width) : base(length, width)
         { }
-
     }
 
     class Square : Figure2D
-
     {
-
         public override double Area { get => Length * Width; }//computed property
-
-        public Square(double length)
-
-        : base(length, length)
-
+        public Square(double length) : base(length, length)
         { }
 
-        public override string ToString()
-
-        => $"Square: length {Length} {Area:F2}";
+        public override string ToString() => $"Square: length {Length} {Area:F2}";
 
     }
 
@@ -182,78 +123,45 @@ namespace Week05
 
 
     class Parent
-
     {
-
         public void Foo() => Console.WriteLine("Foo of Parent");
-
         public virtual void Bar() => Console.WriteLine("virtual Bar of Parent");
-
-
     }
-
     class Child1 : Parent
-
     {
-
     }
-
     class Child2 : Parent
-
     {
-
         public new void Foo() => Console.WriteLine("Foo of Child2"); //hiding the member in the parent
-
     }
 
     class Child3 : Parent
-
     {
-
         public override void Bar() => Console.WriteLine("Bar of Child3"); //replacing the member in the parent (Parent)
-
     }
 
     class Child4 : Child3
-
     {
-
         public override void Bar() => Console.WriteLine("Bar of Child4"); //replacing the member in the parent (Child3)
-
     }
 
     internal class Program
-
     {
-
         static void Main(string[] args)
-
         {
-
             TestMcDonalds();
-
             //TestFigure2D();
-
             //TestHidden();
-
         }
 
         static void TestMcDonalds()
-
         {
-
             Server server = new Server("Gurleen");
-
             Console.WriteLine(server);
-
             Supervisor supervisor = new Supervisor("Sivanes");
-
             Console.WriteLine(supervisor);
-
             Manager manager = new Manager("Iris");
-
             Console.WriteLine(manager);
-
         }
 
 
@@ -277,23 +185,23 @@ namespace Week05
     }
 }
 
-        //static void TestHidden()
+//static void TestHidden()
 
-        //{
+//{
 
-        //    Parent parent = new Parent();
+//    Parent parent = new Parent();
 
-        //    parent.Foo();
-
-
-        //    Child1 child1 = new Child1();
-
-        //    child1.Foo();
+//    parent.Foo();
 
 
-        //    Child2 child2 = new Child2(); //child2 hides member in parent class
+//    Child1 child1 = new Child1();
 
-        //    child2.Foo(); //using the child2 member
+//    child1.Foo();
 
-        //    ((Parent)child2).Foo();//using the parent member
+
+//    Child2 child2 = new Child2(); //child2 hides member in parent class
+
+//    child2.Foo(); //using the child2 member
+
+//    ((Parent)child2).Foo();//using the parent member
 
