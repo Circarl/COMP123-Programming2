6cadb637fe66259c245adf8a985041b7ed96d529
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-02 20:06:13 -0500

    test1 attempt

---
+++ test1_answer/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Enter a sentence: ");
+string sentence = Console.ReadLine();
+
+string[] words = sentence.Split(' ');
+string lastWord = words[words.Length - 1];
+
+int length = lastWord.Length;
+Console.WriteLine(length);
+Console.ReadKey();
