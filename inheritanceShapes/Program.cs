987072febd8e788f9c67a2b52e0627e010f0f123
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-04 08:18:23 -0500

    Update Program.cs

--- inheritanceShapes/Program.cs
+++ inheritanceShapes/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Xml.Linq;
-const double PI = 3.14;
 
 abstract class Shape { 
     protected string Name { get; private set; }
@@ -13,45 +12,24 @@ abstract class Shape {
 class Square : Shape
 {
     protected double Length { get; set; }
-    protected override double Area { get { return Length * 4; } }
+    protected override double Area { get { return (Length * 4); } }
     public Square(string name, double length) : base(name)
      => Length = length;
 }
 
 class Rectangle : Shape
 {
-    //    Rectangle
-    //    Class
-    //   Shape
-
-    //Properties
-    //# <<C# property get, set>>Width : double
-    //# <<C# property get, set>>Height : double
-    //+ <<C# property get>>Area : double
-
-    //Methods
-    //+ <<constructor>> Rectangle(string name, double height, double width)
     public double Width { get; set; }
     public double Height { get; set; }
-    public double Area { get; }
+    protected override double Area { get { return Width * Height; }  }
 
     public Rectangle(string name, double height, double width) : base(name)
-        => (Height, Area) = (height, width);
-
-
+        => (Height, Width) = (height, width);
 }
 class Circle : Square
 {
-    public double Area { get; }
-    public override double Area
-    {
-        get
-        {
-            return PI * Length * Length;
-        }
-    }
-    public Circle(string name, double length) : base(name)
-        => Length = length;
+    public override double Area => Math.PI * Length * Length;
+    public Circle(string name, double length) : base(name, length) => Length = length;
 }
 class Ellipse : Rectangle { }
 class Diamond : Rectangle { }
