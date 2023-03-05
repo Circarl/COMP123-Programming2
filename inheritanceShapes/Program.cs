faa56df267c15b08f2d10a857afde6e9d0da471f
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-04 22:09:46 -0500

    inheritanceShapes updated

--- inheritanceShapes/Program.cs
+++ inheritanceShapes/Program.cs
@@ -16,21 +16,30 @@ class Square : Shape
     public Square(string name, double length) : base(name)
      => Length = length;
 }
-
+class Circle : Square
+{
+    protected override double Area => Math.PI * Length * Length;
+    public Circle(string name, double length) : base(name, length) => Length = length;
+}
 class Rectangle : Shape
 {
     public double Width { get; set; }
     public double Height { get; set; }
-    protected override double Area { get { return Width * Height; }  }
+    protected override double Area { get { return Width * Height; } }
 
     public Rectangle(string name, double height, double width) : base(name)
         => (Height, Width) = (height, width);
 }
-class Circle : Square
+class Ellipse : Rectangle 
 {
-    public override double Area => Math.PI * Length * Length;
-    public Circle(string name, double length) : base(name, length) => Length = length;
+//    Properties
+//+ <<C# property get>>Area : double
+
+//Methods
+//+ <<constructor>> Ellipse(string name, double height, double width)
+    protected override double Area { get => Math.PI * Width * Height; }
+    public Ellipse(string name, double height, double width) : base(name) => (Height, Width) = (height, width);
+
 }
-class Ellipse : Rectangle { }
 class Diamond : Rectangle { }
 class Triangle : Rectangle { }
