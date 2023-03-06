05e5ed410223d8ff987d71acf61f242e51fe8d67
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-05 21:20:41 -0500

    working Code - for submission and final

--- inheritanceShapes/Program.cs
+++ inheritanceShapes/Program.cs
@@ -5,21 +5,23 @@ abstract class Shape
     protected string Name { get; private set; }
     protected abstract double Area { get; }
 
-    public Shape(string name) { }
+    public Shape(string name) => Name = name;
     public override string ToString()
-     => $"{Name} - {Area}";
+     => $"{Name} has an area of {Area:F2}";
 }
 
 class Square : Shape
 {
     protected double Length { get; set; }
     protected override double Area { get { return (Length * 4); } }
+
     public Square(string name, double length) : base(name)
      => Length = length;
 }
 class Circle : Square
 {
     protected override double Area => Math.PI * Length * Length;
+
     public Circle(string name, double length) : base(name, length) => Length = length;
 }
 class Rectangle : Shape
@@ -33,21 +35,47 @@ class Rectangle : Shape
 }
 class Ellipse : Rectangle
 {
-    //    Properties
-    //+ <<C# property get>>Area : double
-
-    //Methods
-    //+ <<constructor>> Ellipse(string name, double height, double width)
     protected override double Area { get => Math.PI * Width * Height; }
-    public Ellipse(string name, double height, double width) : base(name)
-        => (Height, Width) = (height, width);
 
+    public Ellipse(string name, double height, double width) : base(name, height, width)
+    {
+    }
 }
-class Diamond : Rectangle 
+class Triangle : Rectangle
 {
-    
+    protected override double Area => Width * Height * 0.5;
+    public Triangle(string name, double height, double width)
+        : base(name, height, width) { }
 }
-class Triangle : Rectangle 
-{
 
+class Diamond : Rectangle
+{
+    protected override double Area => Width * Height;
+    public Diamond(string name, double height, double width)
+        : base(name, height, width) { }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Square("s1", 2));
+        shapes.Add(new Rectangle("r1", 2, 3));
+        shapes.Add(new Circle("c1", 2));
+        shapes.Add(new Triangle("t1", 4, 6));
+        shapes.Add(new Ellipse("e1", 2, 3));
+        shapes.Add(new Diamond("d1", 2, 3));
+
+        shapes.Add(new Square("s2", 5));
+        shapes.Add(new Rectangle("r2", 5, 4));
+        shapes.Add(new Circle("c2", 1));
+        shapes.Add(new Triangle("t2", 7, 8));
+
+        foreach (var s in shapes)
+        {
+            Console.WriteLine(s);
+        }
+    }
 }
