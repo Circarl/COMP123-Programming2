1743e11fff4c4a66e317d17a60a2d703fef589a8
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-17 23:36:43 -0400

    added new code

--- Fraction_Class/Program.cs
+++ Fraction_Class/Program.cs
@@ -30,32 +30,31 @@ class Fraction
     }
     public static Fraction operator -(Fraction left, Fraction right)
     {
-        bool sameDenominator = (left.Bottom == right.Bottom); //true
-        int top, bottom;
 
-        decimal r1 = (decimal)a.num * b.den - (decimal)b.num * a.den;
-        decimal r2 = (decimal)a.den * b.den;
+        try
 
-        return new Fraction(r1, r2);
-        if (sameDenominator)
         {
-            top = left.Top - right.Top;
-            bottom = left.Bottom;
-            return new Fraction(top, bottom);
-        }
-        else
-        {
-            //(9 / 7) + (3 / 4) = [(9 × 4) + (3 × 7)]/ (7 × 4)
-            top = (left.Top * right.Bottom) + (right.Top * left.Bottom);
-            bottom = (left.Bottom * right.Bottom);
-            return new Fraction(top, bottom);
+            if (right.Top * left.Bottom > left.Top * right.Bottom)
 
+            {
+                throw new Exception("Right value is larger than left value: This conclude the result is Negative");
+            }
+            else
+            {
+                int newTop = left.Top * right.Bottom - left.Bottom * right.Top;
+                int newBottom = left.Bottom * right.Bottom;
+                return new Fraction(newTop, newBottom);
+            }
         }
+        catch (Exception E)
+        {
+            Console.WriteLine(E.Message);
+            return null;
+        }
+
+
     }
-    //public static Fraction operator -(Fraction left, Fraction right)
-    //{
-    //    return (left.Top / left.Bottom) - (right.Top / right.Bottom);
-    //}
+
     public override string ToString()
     {
         return $"({Top}/{Bottom})"; ;
@@ -72,6 +71,24 @@ class Program
 
         Console.WriteLine($"+ operator ~ {f0} + {f1} = {f0 + f1}");
         Console.WriteLine($"+ operator ~ {f0} + {f2} = {f0 + f2} result should be 19/7");
+
+        Console.WriteLine("Addtion Method:");
+        Fraction addLeft = new Fraction(3, 5);
+        Fraction addRight = new Fraction(1, 2);
+        Console.WriteLine(addLeft + addRight);
+        Console.WriteLine();
+
+        Console.WriteLine("Subtraction Method w/ exception:");
+        Fraction minusLeft = new Fraction(1, 2);
+        Fraction minusRight = new Fraction(3, 5);
+        Console.WriteLine(minusLeft - minusRight);
+
+        Console.WriteLine("Subtraction Method w/o exception:");
+        Fraction minusLeftwithException = new Fraction(3, 5);
+        Fraction minusRighttwithException = new Fraction(1, 2);
+        Console.WriteLine(minusLeftwithException - minusRighttwithException);
+
+        Console.ReadKey();
 
     }
 
