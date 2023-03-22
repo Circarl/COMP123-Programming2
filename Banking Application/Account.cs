9650149819f1408712fb552a7a1d5dd853912f40
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-03-21 21:43:55 -0400

    Made coding on banking applications
    created class Accounts, transactions, and Person class

---
+++ Banking Application/Account.cs
@@ -0,0 +1,65 @@
+﻿using Banking_Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Banking_Application
+{
+    abstract class Account
+    {
+        //        Fields
+        private static int CURRENT_NUMBER;
+        protected readonly List<Transaction> transactions = new List<Transaction>();
+        protected readonly List<Person> holders = new List<Person>();
+        public readonly string Number;
+        private static int _LAST_NUMBER = 0;
+        //-$ CURRENT_NUMBER : int
+        //# transactions : List<Transaction>
+        //# holders : List<Person>
+        //+ «readonly» Number : string
+
+        //          Properties
+        public double Balance { get; protected set; }
+        public double LowestBalance { get; protected set; }
+        //+ «property protected setter» Balance : double
+        //+ «property protected setter» LowestBalance : double
+
+        //      Methods
+        public Account(string type, double balance)
+        {
+            Balance = balance;
+            LowestBalance = balance;
+            Number = type + _LAST_NUMBER.ToString();
+            _LAST_NUMBER++;
+        }
+        //+ «constructor» Account(type : string, balance : double)
+        public void AddUser(Person person) {
+            holders.Add(person);
+                }
+        //+ AddUser(person : Person) : void
+        public void Deposit(double amount, Person person) {
+
+            Balance += amount;
+            if (Balance < LowestBalance) { LowestBalance = Balance; }
+            Transaction transact = new Transaction(Number, amount, Balance, person, DateTime.Now);
+            transactions.Add(transact);
+        }
+        //+ Deposit(amount : double, person : Person) : void
+        public bool IsHolder(string name) {
+            foreach (Person person in holders)
+            {
+                if (person.Name == name)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        //+ IsHolder(name : string) : bool
+        //+ PrepareMonthlyReport() : void
+        //+ ToString() : string
+    }
+}
---
+++ Banking Application/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualBasic;
+
+class Person
+{
+    //Fields
+    private string _Password;
+    public readonly string SIN;
+
+    //Properties
+    public  bool IsAuthenticated { get; private set; }
+    public  string Name { get; private set; }
+    //constructor
+    public Person(string name, string sin)
+        => (Name, SIN) = (name,sin);
+    //Methods
+    public void Login (string password) {
+
+        if (password != "admin123") //password set
+        {
+           //throw new AccountException("Invalid password.");
+        }
+        IsAuthenticated = true;
+    }
+    public void Logout()
+    {
+        IsAuthenticated = false;
+    }
+    public override string ToString()
+        => $"{Name} {(IsAuthenticated ? "is authenticated" : "is not authenticated")}";
+
+}
+struct Transaction
+{
+    //properties
+    public string AccountNumber { get; }
+    public double Amount { get; }
+    public double EndBalance { get; }
+    public Person Originator { get; }
+    public DateTime Time { get; }
+
+    //methods
+    public Transaction(string accountNumber, double amount, double endBalance, Person person, DateTime time)
+        => (AccountNumber, Amount, EndBalance, Originator, Time) = (accountNumber, amount, endBalance, person, time);
+
+    public override string ToString()
+        => $"{Time.ToShortTimeString()} {Originator.Name} {Amount:C2} {EndBalance:C2}"; //include withdraw or deposit in the output
+}
+
+class AccountException : Exception
+{
+    //fields
+    public const string ACCOUNT_DOES_NOT_EXIST = "Account does not exist";
+    public const string CREDIT_LIMIT_HAS_BEEN_EXCEEDED = "Credit limit has been exceeded";
+    public const string NAME_NOT_ASSOCIATED_WITH_ACCOUNT = "Name not associated with account";
+    public const string NO_OVERDRAFT = "No overdraft";
+    public const string PASSWORD_INCORRECT = "Incorrect Password";
+    public const string USER_DOES_NOT_EXIST = "User dont exist.";
+    public const string USER_NOT_LOGGED_IN = "User not logged in.";
+    //Properties
+
+    //Methods
+    public AccountException() : base()
+    { 
+    
+    }
+    public AccountException(string reason) : base()
+    {
+
+    }
+
+}
+
