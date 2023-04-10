eb134329ae907fa860ff8d002c913612b4e5131d
cirC4rl <94388078+Circarl@users.noreply.github.com>
2023-04-10 17:54:16 -0400

    added project docs

---
+++ COMP123_Project_Final/COMP123_Project/Student_Integration.cs
@@ -0,0 +1,23 @@
+﻿namespace Group3_Project
+{
+    class Student
+    {
+        // Properties
+        public string StudentName { get; set; }
+        public int StudentID { get; set; }
+        public double GPA { get; set; }
+
+        // Constructor
+        public Student(string studentName, int studentID, double gpa)
+        {
+            StudentName = studentName;
+            StudentID = studentID;
+            GPA = gpa;
+        }
+
+        public override string ToString()
+        {
+            return $"{StudentName,-10} ID# {StudentID,-10} GPA:{GPA,-10:F2}";
+        }
+    }
+}
